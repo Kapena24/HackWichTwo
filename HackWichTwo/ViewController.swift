b9a5eb02b5fede8236d4e3d7139d2a74adf981c1
Kapena Kaaihue <142773521+Kapena24@users.noreply.github.com>
2023-09-07 16:06:27 -1000

    completed part 1-7

--- HackWichTwo/ViewController.swift
+++ HackWichTwo/ViewController.swift
@@ -9,12 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+var welcome : String = "Welcome World! "
+var myApp : String = " My name is Kapena and this is my first app."
+var text: String = ""
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func pressMeButtonPressed(_ sender: Any) {
+    @IBAction func pressMeButtonPressed(_ sender: UIButton) {
         print("Hello World")
     }
     
