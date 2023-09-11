cea36bba6bf0314d00837f73515b4bd4b3ffbcbb
Kapena Kaaihue <142773521+Kapena24@users.noreply.github.com>
2023-09-10 17:48:12 -1000

    Completed set 1-6

---
+++ HackWichTwo/secondViewController.swift
@@ -0,0 +1,23 @@
+//
+//  secondViewController.swift
+//  HackWichTwo
+//
+//  Created by Kapena Kaaihue on 9/10/23.
+//
+
+import UIKit
+
+class secondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func changeColorButtonPressed(_ sender: Any){
+    
+        self.view.backgroundColor = UIColor.blue
+        
+    }
+}
